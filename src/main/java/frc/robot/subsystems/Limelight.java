2d1518daea483f7d8e69713061730fa81b56a7eb
kho25 <91566656+kho25@users.noreply.github.com>
2022-02-02 15:36:49 -0800

    limelight code

--- src/main/java/frc/robot/subsystems/Limelight.java
+++ src/main/java/frc/robot/subsystems/Limelight.java
@@ -6,6 +6,7 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.networktables.NetworkTableEntry;
 import edu.wpi.first.networktables.NetworkTableInstance;
+import java.lang.Math;
 
 /**
  * Wrapper class for getting and setting Limelight NetworkTable values.
@@ -15,6 +16,8 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 public class Limelight {
 	private static NetworkTableInstance table = null;
 
+	double reflectiveTapeLength = 5*2.54; //inches*2.54=cm
+	double knownHeight = 10; //TODO: find value from top height of reflective tape - height of camera point AND convert to inches
 	/**
 	 * Light modes for Limelight.
 	 * 
@@ -131,4 +134,45 @@ public class Limelight {
 
 		return table.getTable("limelight").getEntry(key);
 	}
+
+
+	public static double[] getCorners(){
+		double[] placeHoler = new double [8]; //temporary fix for returning corners. 
+		return getValue("tcornxy").getDoubleArray(placeHoler); //returns array of corner cordinates x0, y0...
+	}
+
+	public static double pixelsToAngle(double pixelXValue){ 
+		if(pixelXValue > 160){
+			pixelXValue = pixelXValue-160;
+		}
+		 return pixelXValue/(320/59.6); //returns angle from center, center is 0 degrees 
+	}
+
+	public static double getHypotenuse(double triangulationLength){ //finds the hypotenuse
+		double distance;
+		double a, b; //angles for triangulation
+		double[] corners = getCorners();
+
+		a = 90 - pixelsToAngle(corners[0]); //hopefully these corners are the top left and right
+		b = 90 - pixelsToAngle(corners[4]);
+
+
+		distance = (triangulationLength*Math.sin(a)*Math.sin(b))/Math.sin(a+b);
+
+		return distance;
+	}
+
+	public static double getAngle(double knownHeight){ // gets angle from ground (0 degrees) to angle from camera to top of reflective tape
+		double sideC = getHypotenuse(5*2.54); //length of reflective tape
+		
+		return Math.asin(knownHeight/sideC);
+	}
+
+	public static double getDistance(double knownHeight){ //distance from camera to net thing with the tape 
+		double sideC = getHypotenuse(5*2.54); //length of reflective tape
+
+		return Math.sqrt((sideC*sideC)-(knownHeight*knownHeight));
+	}
+
+
 }
