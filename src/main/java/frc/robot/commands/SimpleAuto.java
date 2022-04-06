cf727d689ecd7e214cc4d731aa7ac6988390fb80
lknox23 <56613012+lknox23@users.noreply.github.com>
2022-04-05 17:47:28 -0700

    simpleAuto intake stuff

--- src/main/java/frc/robot/commands/SimpleAuto.java
+++ src/main/java/frc/robot/commands/SimpleAuto.java
@@ -1,5 +1,6 @@
 package frc.robot.commands;
 
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.ParallelDeadlineGroup;
 import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
@@ -12,16 +13,13 @@ import frc.robot.subsystems.Vision;
 public class SimpleAuto extends SequentialCommandGroup {
     public SimpleAuto(Drive drive, Indexer indexer, Flywheel flywheel, Vision vision) {
         addCommands(
-            new ParallelDeadlineGroup(
-                new ForwardTimeBased(drive),
-                new RunCommand(() -> {
-                        indexer.setGuide(0.8);
-                        indexer.intake();
-                }, indexer)
-            ),
+            new InstantCommand(() -> {
+                indexer.setGuide(0.8);
+                indexer.intake();
+            }, indexer),
+            new ForwardTimeBased(drive),
             new WaitCommand(1),
-            new RunCommand(() -> {
-                // indexer.setIndexer(0);
+            new InstantCommand(() -> {
                 indexer.setGuide(0);
                 indexer.setIntake(0);
             }, indexer),
